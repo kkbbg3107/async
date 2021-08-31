920dc93f661aef502cc46633a8579568e00bc23c
kane <kkbbg3106@gmail.com>
2021-08-31 08:37:57 +0800

    0831 使用QUEUE解決忙碌主機

--- ClassLibraryAsync/Handler/MaxRequestHandler.cs
+++ ClassLibraryAsync/Handler/MaxRequestHandler.cs
@@ -28,20 +28,10 @@ namespace ClassLibraryAsync.Handler
         /// </summary>
         private SemaphoreSlim semaphores;
 
-        ///// <summary>
-        ///// 執行緒安全字典 key:server的索引 value:server的請求次數
-        ///// </summary>
-        //private ConcurrentDictionary<int, int> dict_Thread = new ConcurrentDictionary<int, int>();
-
         /// <summary>
         /// 存正在忙線的主機
         /// </summary>
-        private ConcurrentQueue<int> queue = new ConcurrentQueue<int>(); 
-
-        /// <summary>
-        /// 初始請求數
-        /// </summary>
-        private int requests = 0;
+        private ConcurrentQueue<int> queue = new ConcurrentQueue<int>();         
 
         /// <summary>
         /// 建立建構式
@@ -67,26 +57,26 @@ namespace ClassLibraryAsync.Handler
         /// <returns>response物件</returns>
         public async Task<Result> GetAsync(ReportObj reportObj)
         {
-            var result = new Result();
-
+            var result = new Result();            
+            
+            // 异步等待进入信号量，如果没有线程被授予对信号量的访问权限，则进入执行保护代码；否则此线程将在此处等待，直到信号量被释放为止
             await semaphores.WaitAsync();
 
             // 拿到閒置主機
             var idleIndex = GetIdleServiceIndex();
-            
-            // 异步等待进入信号量，如果没有线程被授予对信号量的访问权限，则进入执行保护代码；否则此线程将在此处等待，直到信号量被释放为止                               
-            try
-            {
-                // 把主機索引存去queue
-                //queue.Enqueue(idleIndex);
 
+            // 把主機從queue裡面移除 queue 變閒置
+            queue.Enqueue(idleIndex);
+
+            try
+           {                
                 // 做服務!!! 印到CONSOLE上 做完後=> 在釋放資源           
                 result = await allServiceData[idleIndex].GetAsync(reportObj);
 
-                // 把主機從queue裡面移除 queue
-                //queue.TryDequeue(out int res);
+                // 把主機從queue裡面移除 queue 變閒置
+                queue.TryDequeue(out int res);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex);
             };
@@ -100,19 +90,17 @@ namespace ClassLibraryAsync.Handler
         /// <summary>
         /// 取得誰是閒置主機
         /// </summary>
-        /// <returns>主機編號</returns>
+        /// <returns>閒置主機主機編號</returns>
         private int GetIdleServiceIndex()
         {
-            // 只要有閒置
+            // 只要有閒置 忙碌主機數 != 所有主機數
             if (queue.Count != allServiceData.Count)
             {
                 // 找閒置主機
-                return getidleServer();
+                return GetIdleServerRandom();
             }
-            else // 如果沒有閒置server 看哪台先完成就先回傳哪台 WhenAny => 需要找到忙碌到閒置的任務
-            {
-                //var result = 0;
-
+            else // 如果沒有閒置server  需要找到忙碌到閒置的任務
+            {    
                 Task t = Task.Run(() =>
                 {
                     // 都在忙碌
@@ -123,19 +111,25 @@ namespace ClassLibraryAsync.Handler
 
                 });
 
+                // 等待忙碌的時間
                 t.Wait();
 
                 // 回傳主機編號
-                return getidleServer();
+                return GetIdleServerRandom();
             }
         }
 
-        private int getidleServer()
+        /// <summary>
+        /// 透過一直迴圈找閒置主機
+        /// </summary>
+        /// <returns>閒置主機編號</returns>
+        private int GetIdleServerRandom()
         {
             while (true)
             {
                 var server = Random.Next(0, allServiceData.Count);
 
+                // 如果不包含忙碌主機 就回傳主機編號
                 if (!queue.Contains(server))
                 {
                     return server;
