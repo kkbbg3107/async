96c6d5ecf9c3cfc6e1498c3a4411c54eae384df3
kane <kkbbg3106@gmail.com>
2021-09-01 09:45:43 +0800

    0901 非同步過關

--- ClassLibraryAsync/Handler/MaxRequestHandler.cs
+++ ClassLibraryAsync/Handler/MaxRequestHandler.cs
@@ -17,43 +17,34 @@ namespace ClassLibraryAsync.Handler
     /// </summary>
     public class MaxRequestHandler : IReport
     {
-        /// 建立私有服務欄位
-        private readonly List<IReport> allServiceData;
-
         /// <summary>
         /// 建立semaphoreslim鎖
         /// </summary>
-        private readonly SemaphoreSlim semaphores;
+        private readonly SemaphoreSlim Semaphores;
 
         /// <summary>
         /// 存放semaphore的信號量
         /// </summary>
-        private readonly ConcurrentQueue<int> queue = new ConcurrentQueue<int>();      
+        private readonly ConcurrentQueue<IReport> Queue = new ConcurrentQueue<IReport>();      
         
         /// <summary>
         /// 建立建構式
         /// </summary>
         /// <param name="reports">各式服務</param>
         public MaxRequestHandler(List<IReport> allServerObjs, int maxRequetCount)
-        {            
-            allServiceData = allServerObjs;
-
+        {               
             // 信號量為所有主機的request最大總和
-            semaphores = new SemaphoreSlim(maxRequetCount* allServerObjs.Count);
+            Semaphores = new SemaphoreSlim(maxRequetCount* allServerObjs.Count);
+            var random = new Random();
 
             // queue管理semaphoreslim的信號量
             for (int i = 1; i <= maxRequetCount*allServerObjs.Count; i++)
             {
                 // 每一個請求隨機給主機 這邊指定主機
-                var server = Random.Next(0, allServiceData.Count);             
-                queue.Enqueue(server);
+                var server = random.Next(0, allServerObjs.Count);             
+                Queue.Enqueue(allServerObjs[server]);                
             }
         }
-
-        /// <summary>
-        /// 定義隨機變數
-        /// </summary>
-        private Random Random = new Random();
        
         /// <summary>
         /// 假如每個service 的請求數量上限為3 主程式寄發30個請求
@@ -64,23 +55,23 @@ namespace ClassLibraryAsync.Handler
         public async Task<Result> GetAsync(ReportObj reportObj)
         {        
             // 异步等待进入信号量，如果没有线程被授予对信号量的访问权限，则进入执行保护代码；否则此线程将在此处等待，直到信号量被释放为止
-            await semaphores.WaitAsync();
+            await Semaphores.WaitAsync();
                                
             // 拿佇列開頭的資源 並釋放掉
-            queue.TryDequeue(out int res); // 釋放掉的請求
+            Queue.TryDequeue(out IReport res); // 釋放掉的請求
 
             try
             {                
                 // 做服務!!! 印到CONSOLE上 做完後=> 再釋放資源           
-                return await allServiceData[res].GetAsync(reportObj);       
+                return await res.GetAsync(reportObj);       
             }       
             finally
             {
                 // 把資源放回佇列上
-                queue.Enqueue(res);
+                Queue.Enqueue(res);
 
                 // 增加semaphoreSlim內的信號空間 RELEASE +1 池+1    
-                semaphores.Release();
+                Semaphores.Release();
             }           
         }
     }
